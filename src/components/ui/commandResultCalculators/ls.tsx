4ceba7046073813563c3ad79d83126986348e89c
Nelson M. Rios <nelsonmatias9@hotmail.com>
2024-04-22 17:29:19 -0300

    Fix ls behaviour when listing nested files

--- src/components/ui/commandResultCalculators/ls.tsx
+++ src/components/ui/commandResultCalculators/ls.tsx
@@ -6,12 +6,11 @@ const CalculateLsResult = () => {
   let currentTree: Object = fileTree;
 
   const { currentPath } = useContext(CurrentPathContext);
-
   if (currentPath !== "") {
-    const pathParts = currentPath.split(".");
+    const pathParts = currentPath.split("/");
     for (const part of pathParts) {
       type ValidPath = keyof typeof currentTree;
-      if (currentTree[part as ValidPath] === undefined) {
+      if (currentTree[part as ValidPath] !== undefined) {
         currentTree = currentTree[part as ValidPath];
       }
     }
